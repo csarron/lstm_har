de2acd142d1a329a76331caecc84c80c5e44599d
Arron Cao <qqcao.cs@gmail.com>
2017-03-30 00:55:07 -0400

    fix: lstm api

--- lstm.py
+++ lstm.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 import tensorflow as tf
+import tensorflow.contrib.rnn as rnn
 import numpy as np
+from os import listdir
+from os.path import isfile, join
 
 
 def read_data(signal_files):
@@ -30,31 +33,40 @@ def read_label(label_file):
 
     # use one-hot encoding
     y_one_hot = np.zeros((y_.size, y_.max()))
-    y_one_hot[np.arange(y_.size), y_-1] = 1
+    y_one_hot[np.arange(y_.size), y_ - 1] = 1
 
     return y_one_hot
+
+
+def list_files(path):
+    """
+    get the files under a folder
+    :param path: data path
+    :return: a list of data files
+    """
+    return [join(path, data_file) for data_file in listdir(path) if isfile(join(path, data_file))]
 
 
 class Config(object):
     """
     define a class to store parameters,
-    the input should be feature mat of training and testing
+    the input should be feature matrix of training and testing
     """
 
-    def __init__(self, X_train, X_test):
+    def __init__(self, input_data_train, input_data_test):
         # Input data
-        self.train_count = len(X_train)  # 7352 training series
-        self.test_data_count = len(X_test)  # 2947 testing series
-        self.n_steps = len(X_train[0])  # 128 time_steps per series
+        self.train_count = len(input_data_train)  # 7352 training series
+        self.test_data_count = len(input_data_test)  # 2947 testing series
+        self.n_steps = len(input_data_train[0])  # 128 time_steps per series
 
-        # Trainging
+        # Training
         self.learning_rate = 0.0025
         self.lambda_loss_amount = 0.0015
         self.training_epochs = 300
         self.batch_size = 1500
 
         # LSTM structure
-        self.n_inputs = len(X_train[0][0])  # Features count is of 9: three 3D sensors features over time
+        self.n_inputs = len(input_data_train[0][0])  # Features count is of 9: three 3D sensors features over time
         self.n_hidden = 32  # nb of neurons inside the neural network
         self.n_classes = 6  # Final output classes
         self.W = {
@@ -67,50 +79,47 @@ class Config(object):
         }
 
 
-def LSTM_Network(feature_mat, config):
-    """model a LSTM Network,
-      it stacks 2 LSTM layers, each layer has n_hidden=32 cells
-       and 1 output layer, it is a full connet layer
-      argument:
-        feature_mat: ndarray fature matrix, shape=[batch_size,time_steps,n_inputs]
-        config: class containing config of network
-      return:
-              : matrix  output shape [batch_size,n_classes]
+def lstm_net(feature_matrix, conf):
     """
+    model a LSTM Network, it stacks 2 LSTM layers, each layer has n_hidden=32 cells
+    and 1 output layer, it is a full connected layer
+       
+    :param feature_matrix: feature matrix, shape=[batch_size, time_steps, n_inputs]
+    :param conf: config of network
+    :return: output matrix, shape=[batch_size, n_classes]
+    """
+
     # Exchange dim 1 and dim 0
-    feature_mat = tf.transpose(feature_mat, [1, 0, 2])
+    feature_matrix = tf.transpose(feature_matrix, [1, 0, 2])
     # New feature_mat's shape: [time_steps, batch_size, n_inputs]
 
     # Temporarily crush the feature_mat's dimensions
-    feature_mat = tf.reshape(feature_mat, [-1, config.n_inputs])
+    feature_matrix = tf.reshape(feature_matrix, [-1, conf.n_inputs])
     # New feature_mat's shape: [time_steps*batch_size, n_inputs]
 
     # Linear activation, reshaping inputs to the LSTM's number of hidden:
-    hidden = tf.nn.relu(tf.matmul(
-        feature_mat, config.W['hidden']
-    ) + config.biases['hidden'])
+    hidden = tf.nn.relu(tf.matmul(feature_matrix, conf.W['hidden']) + conf.biases['hidden'])
     # New feature_mat (hidden) shape: [time_steps*batch_size, n_hidden]
 
     # Split the series because the rnn cell needs time_steps features, each of shape:
-    hidden = tf.split(0, config.n_steps, hidden)
-    # New hidden's shape: a list of lenght "time_step" containing tensors of shape [batch_size, n_hidden]
+    hidden = tf.split(hidden, conf.n_steps, 0)
+    # New hidden's shape: a list of length "time_step" containing tensors of shape [batch_size, n_hidden]
 
     # Define LSTM cell of first hidden layer:
-    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(config.n_hidden, forget_bias=1.0)
 
     # Stack two LSTM layers, both layers has the same shape
-    lsmt_layers = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * 2)
+    lstm_layers = rnn.MultiRNNCell([rnn.BasicLSTMCell(conf.n_hidden, forget_bias=1.0) for _ in range(2)])
 
     # Get LSTM outputs, the states are internal to the LSTM cells,they are not our attention here
-    outputs, _ = tf.nn.rnn(lsmt_layers, hidden, dtype=tf.float32)
-    # outputs' shape: a list of lenght "time_step" containing tensors of shape [batch_size, n_classes]
+    outputs, _ = rnn.static_rnn(lstm_layers, hidden, dtype=tf.float32)
+    # outputs' shape: a list of length "time_step" containing tensors of shape [batch_size, n_classes]
 
     # Get last time step's output feature for a "many to one" style classifier,
     # as in the image describing RNNs at the top of this page
     lstm_last_output = outputs[-1]
 
     # Linear activation
-    return tf.matmul(lstm_last_output, config.W['output']) + config.biases['output']
+    return tf.matmul(lstm_last_output, conf.W['output']) + conf.biases['output']
 
 
 if __name__ == "__main__":
@@ -118,58 +127,35 @@ if __name__ == "__main__":
     # -----------------------------
     # step1: load and prepare data
     # -----------------------------
-    # Those are separate normalised input features for the neural network
-    INPUT_SIGNAL_TYPES = [
-        "body_acc_x_",
-        "body_acc_y_",
-        "body_acc_z_",
-        "body_gyro_x_",
-        "body_gyro_y_",
-        "body_gyro_z_",
-        "total_acc_x_",
-        "total_acc_y_",
-        "total_acc_z_"
-    ]
-
-    # Output classes to learn how to classify
-    LABELS = [
-        "WALKING",
-        "WALKING_UPSTAIRS",
-        "WALKING_DOWNSTAIRS",
-        "SITTING",
-        "STANDING",
-        "LAYING"
-    ]
-
-    DATA_PATH = "data/"
-    DATASET_PATH = DATA_PATH + "UCI HAR Dataset/"
-    print("\n" + "Dataset is now located at: " + DATASET_PATH)
+    DATA_PATH = "data/UCI HAR Dataset/"
+    SIGNALS = "Inertial Signals/"
     TRAIN = "train/"
     TEST = "test/"
+    LABEL_TRAIN_FILE = "y_train.txt"
+    LABEL_TEST_FILE = "y_test.txt"
 
-    X_train_signals_paths = [
-        DATASET_PATH + TRAIN + "Inertial Signals/" + signal + "train.txt" for signal in INPUT_SIGNAL_TYPES
-    ]
-    X_test_signals_paths = [
-        DATASET_PATH + TEST + "Inertial Signals/" + signal + "test.txt" for signal in INPUT_SIGNAL_TYPES
-    ]
-    X_train = read_data(X_train_signals_paths)
-    X_test = read_data(X_test_signals_paths)
+    input_data_path_train = DATA_PATH + TRAIN + SIGNALS
+    input_data_path_test = DATA_PATH + TEST + SIGNALS
 
-    y_train_path = DATASET_PATH + TRAIN + "y_train.txt"
-    y_test_path = DATASET_PATH + TEST + "y_test.txt"
-    y_train = read_label(y_train_path)
-    y_test = read_label(y_test_path)
+    input_data_train_files = list_files(input_data_path_train)
+    input_data_test_files = list_files(input_data_path_test)
+
+    if __debug__:
+        print input_data_train_files
+        print input_data_test_files
+
+    input_train = read_data(input_data_train_files)
+    input_test = read_data(input_data_test_files)
+
+    label_train_file = DATA_PATH + TRAIN + LABEL_TRAIN_FILE
+    label_test_file = DATA_PATH + TEST + LABEL_TEST_FILE
+    y_train = read_label(label_train_file)
+    y_test = read_label(label_test_file)
 
     # -----------------------------------
     # step2: define parameters for model
     # -----------------------------------
-    config = Config(X_train, X_test)
-    print("Some useful info to get an insight on dataset's shape and normalisation:")
-    print("features shape, labels shape, each features mean, each features standard deviation")
-    print(X_test.shape, y_test.shape,
-          np.mean(X_test), np.std(X_test))
-    print("the dataset is therefore properly normalised, as expected.")
+    config = Config(input_train, input_test)
 
     # ------------------------------------------------------
     # step3: Let's get serious and build the neural network
@@ -177,54 +163,37 @@ if __name__ == "__main__":
     X = tf.placeholder(tf.float32, [None, config.n_steps, config.n_inputs])
     Y = tf.placeholder(tf.float32, [None, config.n_classes])
 
-    pred_Y = LSTM_Network(X, config)
+    predicted_label = lstm_net(X, config)
 
     # Loss,optimizer,evaluation
-    l2 = config.lambda_loss_amount * \
-         sum(tf.nn.l2_loss(tf_var) for tf_var in tf.trainable_variables())
+    l2 = config.lambda_loss_amount * sum(tf.nn.l2_loss(tf_var) for tf_var in tf.trainable_variables())
     # Softmax loss and L2
-    cost = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(pred_Y, Y)) + l2
-    optimizer = tf.train.AdamOptimizer(
-        learning_rate=config.learning_rate).minimize(cost)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=predicted_label, labels=Y)) + l2
+    optimizer = tf.train.AdamOptimizer(learning_rate=config.learning_rate).minimize(cost)
 
-    correct_pred = tf.equal(tf.argmax(pred_Y, 1), tf.argmax(Y, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_pred, dtype=tf.float32))
+    correct_predicted = tf.equal(tf.argmax(predicted_label, 1), tf.argmax(Y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_predicted, dtype=tf.float32))
 
     # --------------------------------------------
-    # step4: Hooray, now train the neural network
+    # step4: Train the neural network
     # --------------------------------------------
     # Note that log_device_placement can be turned ON but will cause console spam.
     sess = tf.InteractiveSession(config=tf.ConfigProto(log_device_placement=False))
-    tf.initialize_all_variables().run()
+    tf.global_variables_initializer().run()
 
     best_accuracy = 0.0
     # Start training for each batch and loop epochs
     for i in range(config.training_epochs):
         for start, end in zip(range(0, config.train_count, config.batch_size),
                               range(config.batch_size, config.train_count + 1, config.batch_size)):
-            sess.run(optimizer, feed_dict={X: X_train[start:end],
-                                           Y: y_train[start:end]})
 
+            sess.run(optimizer, feed_dict={X: input_train[start:end], Y: y_train[start:end]})
         # Test completely at every epoch: calculate accuracy
-        pred_out, accuracy_out, loss_out = sess.run([pred_Y, accuracy, cost], feed_dict={
-            X: X_test, Y: y_test})
-        print("traing iter: {},".format(i) + \
-              " test accuracy : {},".format(accuracy_out) + \
-              " loss : {}".format(loss_out))
+        predict_out, accuracy_out, loss_out = sess.run([predicted_label, accuracy, cost],
+                                                       feed_dict={X: input_test, Y: y_test})
+        print("training iter: {},".format(i)
+              + " test accuracy : {},".format(accuracy_out)
+              + " loss : {}".format(loss_out))
         best_accuracy = max(best_accuracy, accuracy_out)
 
-    print("")
-    print("final test accuracy: {}".format(accuracy_out))
     print("best epoch's test accuracy: {}".format(best_accuracy))
-    print("")
-
-    # ------------------------------------------------------------------
-    # step5: Training is good, but having visual insight is even better
-    # ------------------------------------------------------------------
-    # The code is in the .ipynb
-
-    # ------------------------------------------------------------------
-    # step6: And finally, the multi-class confusion matrix and metrics!
-    # ------------------------------------------------------------------
-    # The code is in the .ipynb
